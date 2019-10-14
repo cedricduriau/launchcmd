1c7188fedd0a66e45fa06ac57080fa8f2ae55d7c
Cedric Duriau <duriau.cedric@live.be>
2019-10-15 00:04:33 +0200

    add docstrings; cleanup

--- python/launchcmd/gitutils.py
+++ python/launchcmd/gitutils.py
@@ -3,7 +3,17 @@ import os
 import subprocess
 
 
+# =============================================================================
+# private
+# =============================================================================
 def _run_check_output(cmd):
+    """Runs a command and returns its output.
+
+    :param cmd: Command to run.
+    :type cmd: str
+
+    :rtype: list[str]
+    """
     # check output
     output = subprocess.check_output(cmd, shell=True)
 
@@ -18,7 +28,15 @@ def _run_check_output(cmd):
     return lines
 
 
+# =============================================================================
+# public
+# =============================================================================
 def validate_repository_dir(repository_dir):
+    """Validates a repository has a .git directory.
+
+    :param repository_dir: Directory of a repository.
+    :type repository_dir: str
+    """
     contents = os.listdir(repository_dir)
     if ".git" not in contents:
         msg = "following directory is not a git repository, no .git directory found: {}"
@@ -26,30 +44,60 @@ def validate_repository_dir(repository_dir):
 
 
 def get_tags(repository_dir):
+    """Returns all tags of a repository.
+
+    :param repository_dir: Directory of a repository.
+    :type repository_dir: str
+
+    :rtype: list[str]
+    """
     cmd = "cd {} && git tag".format(repository_dir)
     tags = _run_check_output(cmd)
     return tags
 
 
 def get_status(repository_dir):
+    """Returns the unstaged changes of a repository,
+
+    :param repository_dir: Directory of a repository.
+    :type repository_dir: str
+
+    :rtype: list[str]
+    """
     cmd = "cd {} && git status --porcelain".format(repository_dir)
     status = _run_check_output(cmd)
     return status
 
 
 def create_tag(repository_dir, tag_name):
+    """Creates a new tag in a repository.
+
+    :param repository_dir: Directory of a repository.
+    :type repository_dir: str
+
+    :param tag_name: Name of the tag to create.
+    :type tag_name: str
+    """
     cmd = "cd {} && git tag {}".format(repository_dir, tag_name)
     _run_check_output(cmd)
 
 
-def clone_repository(repository_dir, clone_dir):
-    # ensure the cmd looks like cp -R $repository_dir/* $clone_dir/
+def clone_repository(repository_dir, target_dir):
+    """Copies the repository to a target directory.
+
+    :param repository_dir: Directory of the repository to copy/clone.
+    :type repository_dir: str
+
+    :param target_dir: Directory to copy/clone the repository content in.
+    :type target_dir: str
+    """
+    # ensure the cmd looks like cp -R $repository_dir/* $target_dir/
     if not repository_dir.endswith(os.sep):
         repository_dir += os.sep
     repository_dir += "*"
 
-    if not clone_dir.endswith(os.sep):
-        clone_dir += os.sep
+    if not target_dir.endswith(os.sep):
+        target_dir += os.sep
 
-    cmd = "cp -R {} {}".format(repository_dir, clone_dir)
+    cmd = "cp -R {} {}".format(repository_dir, target_dir)
     _run_check_output(cmd)
