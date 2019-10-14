8a180e47266cc1d5d2bd11012928c9e069024ad2
Cedric Duriau <duriau.cedric@live.be>
2019-10-15 00:06:35 +0200

    add docstrings

--- python/launchcmd/moduleutils.py
+++ python/launchcmd/moduleutils.py
@@ -4,12 +4,24 @@ import glob
 
 
 def get_modules_from_directory(directory):
+    """Returns the module files of a directory.
+
+    :param directory: Directory to get module files from.
+    :type directory: str
+
+    :rtype: list[str]
+    """
     glob_path = os.path.join(directory, "*.module")
     module_files = glob.glob(glob_path)
     return module_files
 
 
 def validate_directory_has_module(directory):
+    """Validates a directory has exactly one module file.
+
+    :param directory: Directory to valdiate.
+    :type directory: str
+    """
     module_files = get_modules_from_directory(directory)
 
     if not module_files:
@@ -22,6 +34,13 @@ def validate_directory_has_module(directory):
 
 
 def get_module_from_directory(directory):
+    """Returns the module file of a directory.
+
+    :param directory: Directory to get module files from.
+    :type directory: str
+
+    :rtype: str
+    """
     validate_directory_has_module(directory)
     module_files = get_modules_from_directory(directory)
     return module_files[0]
