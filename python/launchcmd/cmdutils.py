0c60dc9e730ed5c59b0b936db84dbd6815c5bf26
Cedric Duriau <duriau.cedric@live.be>
2019-10-17 17:34:17 +0200

    WIP module un/load

--- python/launchcmd/cmdutils.py
+++ python/launchcmd/cmdutils.py
@@ -1,6 +1,10 @@
+# stdlib modules
+import os
+
 # tool modules
 from launchcmd import envvarutils
 from launchcmd import pathutils
+from launchcmd import moduleutils
 
 
 # =============================================================================
@@ -58,6 +62,25 @@ def build_launchcmd_command(dir_level):
 
     # get all parent levels related to given level
     level_dirs = pathutils.get_level_directories(dir_level)
+
+    # unload previously loaded modules
+    snaphot_loaded_modules = moduleutils.get_snapshot_loaded_modules()
+    all_loaded_modules = moduleutils.get_all_loaded_modules()
+    modules_to_unload = set(snaphot_loaded_modules) - set(all_loaded_modules)
+    for modulefile in modules_to_unload:
+        moduleutils.unload_module(modulefile)
+
+    # snapshot loaded modules
+    all_loaded_modules = moduleutils.get_all_loaded_modules()
+    all_loaded_modules_str = os.pathsep.join(all_loaded_modules)
+    snapshot_cmd = "export _LAUNCHCMD_LOADEDMODULES_SNAPSHOT={}".format(all_loaded_modules_str)
+    commands.append(snapshot_cmd)
+
+    # load installed modules
+    modules_to_load = map(moduleutils.get_modules_from_level, level_dirs)
+    modules_to_load = list(filter(None, modules_to_load))
+    for modulefile in modules_to_load:
+        moduleutils.load_module(modulefile)
 
     # build env vars to unset
     unset_env_vars = envvarutils.get_env_vars_to_unset()
