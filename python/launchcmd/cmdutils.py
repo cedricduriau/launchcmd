c0b7924cbdc93c7a87c4cdef6a868fd5f2cd7402
Cedric Duriau <duriau.cedric@live.be>
2019-11-06 00:01:41 +0100

    implement build launchcmd command

--- python/launchcmd/cmdutils.py
+++ python/launchcmd/cmdutils.py
@@ -1,9 +1,7 @@
-# stdlib modules
-import os
-
 # tool modules
 from launchcmd import envvarutils
 from launchcmd import pathutils
+from launchcmd import packageutils
 from launchcmd import moduleutils
 
 
@@ -22,8 +20,7 @@ def _build_cd_command(path):
 
 
 def _build_set_env_vars_command(env_vars):
-    """
-    Builds the command to set environment variables.
+    """Builds the command to set environment variables.
 
     :param env_vars: Environment variables to set.
     :type env_vars: dict[str, str]
@@ -35,8 +32,7 @@ def _build_set_env_vars_command(env_vars):
 
 
 def _build_unset_env_vars_command(env_vars):
-    """
-    Builds the command to unset environment variables.
+    """Builds the command to unset environment variables.
 
     :param env_vars: Environment variables to unset.
     :type env_vars: dict[str, str]
@@ -44,6 +40,30 @@ def _build_unset_env_vars_command(env_vars):
     :rtype: str
     """
     commands = ["unset {}".format(k) for k in env_vars]
+    return " && ".join(commands)
+
+
+def _build_module_load_command(module_files):
+    """Builds the command to load modules.
+
+    :param module_files: Module files to load.
+    :type module_files: list[str]
+
+    :rtype: str
+    """
+    commands = ["module load {}".format(f) for f in module_files]
+    return " && ".join(commands)
+
+
+def _build_module_unload_command(module_files):
+    """Builds the command to unload modules.
+
+    :param module_files: Module files to unload.
+    :type module_files: list[str]
+
+    :rtype: str
+    """
+    commands = ["module unload {}".format(f) for f in module_files]
     return " && ".join(commands)
 
 
@@ -63,40 +83,59 @@ def build_launchcmd_command(level_dir):
     # get all parent levels related to given level
     level_dirs = pathutils.get_level_directories(level_dir)
 
-    # unload previously loaded modules
-    snaphot_loaded_modules = moduleutils.get_snapshot_loaded_modules()
-    all_loaded_modules = moduleutils.get_all_loaded_modules()
-    modules_to_unload = set(snaphot_loaded_modules) - set(all_loaded_modules)
-    for modulefile in modules_to_unload:
-        moduleutils.unload_module(modulefile)
-
-    # snapshot loaded modules
-    all_loaded_modules = moduleutils.get_all_loaded_modules()
-    all_loaded_modules_str = os.pathsep.join(all_loaded_modules)
-    snapshot_cmd = "export _LAUNCHCMD_LOADEDMODULES_SNAPSHOT={}".format(all_loaded_modules_str)
-    commands.append(snapshot_cmd)
-
-    # load installed modules
-    modules_to_load = map(moduleutils.get_modules_from_level, level_dirs)
-    modules_to_load = list(filter(None, modules_to_load))
-    for modulefile in modules_to_load:
-        moduleutils.load_module(modulefile)
-
-    # build env vars to unset
-    unset_env_vars = envvarutils.get_env_vars_to_unset()
-    unset_env_vars_command = _build_unset_env_vars_command(unset_env_vars)
-    commands.append(unset_env_vars_command)
-
-    # build env vars to set
-    set_env_vars = envvarutils.get_env_vars_to_set(level_dirs)
-    set_env_vars_command = _build_set_env_vars_command(set_env_vars)
-    commands.append(set_env_vars_command)
-
-    # TODO: build PS1 location as prefix in bold
-
     # build cd command
-    cmd_command = _build_cd_command(level_dir)
-    commands.append(cmd_command)
+    cd_command = _build_cd_command(level_dir)
+    commands.append(cd_command)
+
+    # get level vars to set
+    level_env_vars_to_set = envvarutils.build_level_env_vars(level_dirs)
+
+    # get cached level vars to set
+    cached_level_env_vars_to_set = envvarutils.build_cached_env_vars(level_env_vars_to_set)
+
+    # get level vars to unset
+    level_env_vars_set = envvarutils.get_level_env_vars()
+    level_env_vars_to_unset = set(level_env_vars_set.keys()) - set(level_env_vars_to_set.keys())
+
+    # get cached level vars to unset
+    cached_level_env_vars_set = envvarutils.get_cached_level_env_vars()
+    cached_level_env_vars_to_unset = set(cached_level_env_vars_set.keys()) - set(cached_level_env_vars_to_set.keys())
+
+    # build unset level env vars cmd
+    unset_level_env_vars = _build_unset_env_vars_command(level_env_vars_to_unset)
+    commands.append(unset_level_env_vars)
+
+    # build unset cached level env vars cmd
+    unset_cached_level_env_vars = _build_unset_env_vars_command(cached_level_env_vars_to_unset)
+    commands.append(unset_cached_level_env_vars)
+
+    # build set level env vars cmd
+    set_level_env_vars = _build_set_env_vars_command(level_env_vars_to_set)
+    commands.append(set_level_env_vars)
+
+    # build set cached level env vars cmd
+    set_cached_level_env_vars = _build_set_env_vars_command(cached_level_env_vars_to_set)
+    commands.append(set_cached_level_env_vars)
+
+    # get modules to load
+    module_files_to_load = []
+    for _level_dir in level_dirs:
+        package_dirs = packageutils.get_installed_packages_directories(_level_dir)
+        for packge_dir in package_dirs:
+            _module_files = moduleutils.get_modules_from_directory(packge_dir)
+            module_files_to_load.extend(_module_files)
+
+    # get modules to unload
+    module_files_to_unload = envvarutils.get_cached_modules()
+    module_files_to_unload = list(set(module_files_to_unload) - set(module_files_to_load))
+
+    # build module unload cmd
+    module_unload_cmd = _build_module_unload_command(module_files_to_unload)
+    commands.append(module_unload_cmd)
+
+    # build module load cmd
+    module_load_cmd = _build_module_load_command(module_files_to_load)
+    commands.append(module_load_cmd)
 
     # build command string
     commands = filter(None, commands)
