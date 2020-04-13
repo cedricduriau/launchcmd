9d6a0991a2e049a9294cd1df8c16da66cd1fb505
Cedric Duriau <duriau.cedric@live.be>
2020-04-13 19:33:57 +0200

    add docs

--- python/launchcmd/moduleutils.py
+++ python/launchcmd/moduleutils.py
@@ -6,6 +6,14 @@ from launchcmd import packageutils
 
 
 def get_module_file(path):
+    """
+    Return the package module.
+
+    :param path: Directory of a repository, local, released or installed.
+    :type path: str
+
+    :rtype: str
+    """
     package = packageutils.get_package_name(path)
     path = os.path.join(path, package + ".module")
     return path
