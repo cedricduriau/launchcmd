46709127526e6f305b2d89e1c7e0f6da56ab0808
Cedric Duriau <duriau.cedric@live.be>
2020-04-13 19:11:05 +0200

    improve install workflow

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 # stdlib modules
-from setuptools import setup, find_packages
+import os
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+# tool modules
+f = os.path.abspath(__file__)
+package_dir = os.path.join(os.path.dirname(f), "python")
+sys.path.insert(0, package_dir)
+from launchcmd import __version__  # noqa
+
+requirements_dev = ["flake8", "radon"]
 
 
 setup(name="launchcmd",
-      version="0.1.0",
-      description="Lightweight software packaging and deployment tool.",
-      license="GNU GPLv3",
+      version=__version__,
+      description="Lightweight software and enviroment manager.",
+      license="GPLv3",
       author="C&eacute;dric Duriau",
       author_email="duriau.cedric@live.be",
       url="https://github.com/cedricduriau/launchcmd",
       packages=find_packages(where="python"),
       package_dir={"": "python"},
-      scripts=["bin/_launchcmd", "bin/releasepackage"])
+      scripts=["bin/launchcmd"],
+      extras_require={"dev": requirements_dev},
+      data_files=[(os.path.expanduser("~/.launchcmd/released"), [])])
