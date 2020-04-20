c23483d303a95b2578c3eb155690dbd49e9c59e0
Cedric Duriau <duriau.cedric@live.be>
2020-04-20 22:42:01 +0200

    implement bash file to source on profile

---
+++ etc/profile.d/launchcmd.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+function __launch () {
+    # build command line
+    local cmd=`launchcmd printlaunchcmd`
+    if [ ! -z $1]
+    then
+        cmd="$cmd -l $1"
+    fi
+    echo $cmd
+
+    # run command line
+    # eval $cmd
+}
+
+function __land () {
+    # build command line
+    local cmd=`launchcmd printlandcmd`
+    echo $cmd
+
+    # run command line
+    # eval $cmd
+}
+
+
+function launch () {
+    __launch
+
+    function land () {
+        __land
+        unset -f land
+    }
+
+    export -f land
+}
+
+export -f launch
