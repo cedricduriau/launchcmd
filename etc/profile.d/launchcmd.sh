89db163785dd631c3acf94187046b036de91032f
Cedric Duriau <duriau.cedric@live.be>
2020-04-20 23:25:57 +0200

    implement directory switching

--- etc/profile.d/launchcmd.sh
+++ etc/profile.d/launchcmd.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 
 function __launch {
+    # store current location
+    export LCMD_START_LOCATION=`pwd`
+
     # build command line
     local cmd="launchcmd printlaunchcmd"
     if [ ! -z $1 ]; then
         cmd="$cmd -l $1"
+        cd $1
     fi
 
     # run command line
@@ -12,6 +16,10 @@ function __launch {
 }
 
 function __land {
+    echo $LCMD_START_LOCATION
+    cd $LCMD_START_LOCATION
+    unset LCMD_START_LOCATION
+
     # build command line
     local cmd="launchcmd printlandcmd"
 
