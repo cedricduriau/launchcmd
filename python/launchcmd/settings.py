a5dd2f351242375da67e505e2138956abc12b557
Cedric Duriau <duriau.cedric@live.be>
2020-04-20 23:15:17 +0200

    rename env vars prefix to LCMD

--- python/launchcmd/settings.py
+++ python/launchcmd/settings.py
@@ -6,6 +6,6 @@ def abs(path):
     return os.path.abspath(os.path.expanduser(path))
 
 
-ROOT = abs(os.getenv("LC_ROOT_DIR", "~/.launchcmd"))
+ROOT = abs(os.getenv("LCMD_ROOT_DIR", "~/.launchcmd"))
 RELEASE_ROOT = os.path.join(ROOT, "released")
-CURRENT_LOCATION = abs(os.getenv("LC_CURRENT_LOCATION", ""))
+CURRENT_LOCATION = abs(os.getenv("LCMD_CURRENT_LOCATION", ""))
